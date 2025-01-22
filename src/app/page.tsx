a53eb905aa7a3184d8941f2ac518be9392ed5aee
chengivan <chengoleivan@gmail.com>
2025-01-23 00:30:51 +0300

    Renamed classnames in home page and styles files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import styles from "./page.module.css";
+import "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
+    <div className="home-container">
+      <main className="home-main">
+        <div className="home-title-container">
+          <h2 className="home-main-title">Hello, Nisena Agent. Welcome.</h2>
+          <h6 className="home-sub-title">Login to continue</h6>
+        </div>
+
+        <div className="home-sign-in-button">
+          <div><a href="/login">Sign In</a></div>
+        </div>
+      </main>
     </div>
   );
 }
