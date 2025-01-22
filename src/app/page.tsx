8a784a7289f8bacd2a28af3213d9a3ba5cfa8d53
chengivan <chengoleivan@gmail.com>
2025-01-23 00:49:25 +0300

    Added styles to home-sign-in-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import "./main.css";
 
 export default function Home() {
   return (
-      <main className="home-main">
-        <div className="home-title-container">
-          <h2 className="home-main-title">Hello, Nisena Agent. Welcome.</h2>
-          <h6 className="home-sub-title">Login to continue</h6>
-        </div>
+    <main className="home-main">
+      <div className="home-title-container">
+        <h2 className="home-main-title">Hello, Nisena Agent. Welcome.</h2>
+        <h6 className="home-sub-title">Login to continue</h6>
+      </div>
 
+      <div className="home-sign-in-button-container">
         <div className="home-sign-in-button">
-          <div><a href="/login">Sign In</a></div>
+          <a href="/login">Sign In</a>
         </div>
-      </main>
+      </div>
+    </main>
   );
 }
