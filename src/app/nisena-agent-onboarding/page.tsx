eac455e5b417bbad79290d7f87322db5549f4245
chengivan <chengoleivan@gmail.com>
2025-01-25 20:09:16 +0300

    Using useRouter from next/navigation instead of the legacy next/router

--- src/app/nisena-agent-onboarding/page.tsx
+++ src/app/nisena-agent-onboarding/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { supabase } from "../../../utils/supabase/supabaseClient";
 
 export default function Onboarding() {
