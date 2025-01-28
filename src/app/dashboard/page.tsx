2aa9b2c43636bf9dfc9dfb2acc3700cf89f0d2d9
chengivan <chengoleivan@gmail.com>
2025-01-28 21:23:38 +0300

    imported dashboard.css

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { supabase } from "../../../utils/supabase/supabaseClient";
+import "./dashboard.css"
 
 const Dashboard: React.FC = () => {
   const [user, setUser] = useState<any>(null);
